a9730586b404a0274a714374d7b9f18d00ecaf17
Darya Hudzovska <1bestboost@gmail.com>
2022-09-03 20:03:40 +0300

    add axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // const axios = require('axios').default;
 // const axios = require('axios');
-
+import Axios from 'axios';
 import Notiflix from 'notiflix';
 Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
 Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+// key (required)	str	Your API key: 29692752-5f9a27c26e6deec7970509d3f
