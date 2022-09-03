1814aed2a6ad2094f6af41542f6d2d82afb0cac5
Darya Hudzovska <1bestboost@gmail.com>
2022-09-03 19:55:07 +0300

    basic file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// const axios = require('axios').default;
+// const axios = require('axios');
+
+import Notiflix from 'notiflix';
+Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
